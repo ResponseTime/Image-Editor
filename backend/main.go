a92d475b09727b78ff46e5683bce606f16e58814
Looper <aayush.bhattacharjee2002@gmail.com>
2023-12-11 01:16:40 +0530

    .

--- backend/main.go
+++ backend/main.go
@@ -4,17 +4,16 @@ import (
 	"fmt"
 
 	"github.com/joho/godotenv"
-	"go.mongodb.org/mongo-driver/mongo"
 )
-var client *mongo.Client
+
 func main() {
 	err := godotenv.Load()
 	if err != nil {
 		fmt.Println("Error loading .env file")
 	}
 	connectDb()
-	
+
 	router := setupRouter()
-	
+
 	router.Run(":8080")
 }
